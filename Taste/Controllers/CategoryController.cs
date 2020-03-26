16c97a8c14bb8c397bf5680a585b18e91d6babac
Graaf69 <50245238+Graaf69@users.noreply.github.com>
2020-03-26 22:38:43 +0200

    Category changes made

---
+++ Taste/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.Category.GetAll() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id )
+        {
+            var objectFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (objectFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Category.Remove(objectFromDb);
+
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Done!" });
+        }
+    }
+}
